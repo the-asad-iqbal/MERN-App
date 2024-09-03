4c0800b8235333fdeeecfe604e85c4b6b4694eae
M Asad Rizvi <asadraoof023@gmail.com>
2024-09-03 07:08:51 +0500

    init: New Changes

--- server/app.js
+++ server/app.js
@@ -1,7 +1,24 @@
 import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+import { connectDB } from "./config/db.js";
 
 const app = express();
+dotenv.config();
 
-app.listen(3000, () => {
-   console.log("Server started on port 3000");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+   cors({
+      origin: ["http://localhost:5173"],
+      credentials: true,
+      methods: ["GET", "POST", "PUT", "DELETE"],
+   })
+);
+
+connectDB();
+
+app.listen(process.env.PORT, () => {
+   console.log(`Server running on port ${process.env.PORT}`);
 });
